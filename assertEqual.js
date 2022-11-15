fefe72293e072552fe3fe57c9c19b00c5f0a072b
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-14 20:07:45 -0500

    emjoi error codes

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  const errorMsg = "🔴🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected;
+  if (actual === expected) {
+    return "✅✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return errorMsg;
+  }
+};
