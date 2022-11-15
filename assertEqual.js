e64bda4fed87d3b3a0c3e4bb4bab322c5992738b
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-14 20:29:50 -0500

    updated to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
-  const errorMsg = "🔴🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected;
+  const errorMsg = `🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`;
   if (actual === expected) {
-    return "✅✅✅✅ Assertion Passed: " + actual + " === " + expected;
+    return `✅✅✅✅ Assertion Passed:  ${actual} ===  ${expected}`;
   } else {
     return errorMsg;
   }
