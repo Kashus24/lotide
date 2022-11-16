a89a9bec3369504efa78e5f99022b7e593aa5488
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-16 16:45:58 -0500

    removed extras

--- countOnly.js
+++ countOnly.js
@@ -20,38 +20,3 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return counted;
 };
-/*
-const countOnly = function(allItems, itemsToCount) {
-  let counted = {};
-  for (item of allItems) {
-    if(itemsToCount[item]) {
-      if(!counted[item]) {
-        counted[item] = 1;
-      } else {
-        counted[item] += 1;
-      }
-    } 
-  
-  } 
-  return counted;
-};
-*/
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-//assertEqual(result1["Agouhanna"], undefined);
