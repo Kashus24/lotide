80b39b0b4f73be9dacf932e8221bb53713d13cdc
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-16 23:57:57 -0500

    function for comparing objects

---
+++ eqObjects.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  const errorMsg = `🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log( `✅✅✅✅ Assertion Passed:  ${actual} ===  ${expected}`);
+  } else {
+    console.log(errorMsg);
+  }
+};
+const eqArrays = function(first, second) {
+  if (first === second) {
+    return true;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i])
+    return false;
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false;
+    }
+    for (let list of Object.keys(object1)) {
+      if (Array.isArray(object1[list]) || Array.isArray(object2[list])) {
+        if ( !eqArrays(object1[list],object2[list]) ) {
+          return false;
+        }
+      }
+      else if (object1[list] !== object2[list]) {
+        return false;
+      }
+    }
+    return true;
+  };
+    
