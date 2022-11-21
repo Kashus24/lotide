9fbf2746636918ba883281ea87a428b66015a419
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-21 15:00:18 -0500

    updated to use chai, mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,20 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const array = ["one", "two", "three", "four"];
 
-assertEqual(tail(array),"four");
+// const array = ["one", "two", "three", "four"];
+
+// assertEqual(tail(array),"four");
+
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+
+  it("returns '3' for ['3']", () => {
+    assert.strictEqual(tail(['5']), '5'); 
+  });
+
+
+});
