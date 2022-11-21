882d8205f3444ce5618c60d74133206304000643
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-21 00:55:23 -0500

    updated tail to export

--- tail.js
+++ tail.js
@@ -1,14 +1,12 @@
-const assertEqual = function(actual, expected) {
-  const errorMsg = `🔴🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(`✅✅✅✅ Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    console.log(errorMsg);
-  }
-};
-
 
 const tail = function(data) {
-  console.log(data.splice(1));
+
+  const spliced = data[data.length - 1];
+  return spliced;
 };
 
+
+
+
+
+module.exports = tail;
